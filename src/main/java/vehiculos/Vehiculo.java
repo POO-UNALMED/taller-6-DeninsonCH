36485b9a0a05560cabd8573dfd7f867f76e2ce21
DeninsonCH <69688198+DeninsonCH@users.noreply.github.com>
2020-10-06 02:49:50 -0500

    r

--- src/main/java/vehiculos/Vehiculo.java
+++ src/main/java/vehiculos/Vehiculo.java
@@ -1,92 +1,90 @@
 package vehiculos;
 
+import java.util.*;
 public class Vehiculo {
-	static int Cvehiculo = 0;
-	protected String placa;
-	protected int puertas;
-	protected int velocidadMaxima;
-	protected String nombre;						/*nombre vehiculo*/
-	protected int precio;
-	protected int peso;							/*cuanto pesa*/
-	protected String traccion;
-	
+	public String placa;
+	public int puertas;
+	public int velocidadMaxima;
+	public String nombre;
+	public int precio;
+	public int peso;
+	public String traccion;
 	public Fabricante fabricante;
-	
-	
-	/*construtores*/
-	public void vehiculo(){Cvehiculo++;}
-	public Vehiculo(String placa,int puerta,int velocidadMaxima,String nombre,int precio,int peso,String traccion,Fabricante fabricante) {
-		this.placa=placa;
-		this.puertas=puerta;
-		this.velocidadMaxima=velocidadMaxima;
-		this.nombre=nombre;
-		this.precio=precio;
-		this.peso=peso;
-		this.traccion=traccion;
-		this.fabricante=fabricante;
-		Cvehiculo++;
-	}
+	public static int CantidadVehiculos=0;
 	public String getPlaca() {
 		return placa;
+	}
+	
+	
+	
+	public Vehiculo(String placa, int puertas, int velocidadMaxima, String nombre, int precio, int peso,
+			String traccion, Fabricante fabricante) {
+		this.placa = placa;
+		this.puertas = puertas;
+		this.velocidadMaxima = velocidadMaxima;
+		this.nombre = nombre;
+		this.precio = precio;
+		this.peso = peso;
+		this.traccion = traccion;
+		this.fabricante = fabricante;
+		CantidadVehiculos++;
+		}
+	
+	public void setPlaca(String placa) {
+		this.placa = placa;
 	}
 	public int getPuertas() {
 		return puertas;
 	}
-	public int getVelocidadMaxima() {
-		return velocidadMaxima;
-	}
-	public String getNombre() {
-		return nombre;
-	}
-	public int getPrecio() {
-		return precio;
-	}
-	public int getPeso() {
-		return peso;
-	}
-	public String getTraccion() {
-		return traccion;
-	}
-	public Fabricante getFabricante() {
-		return fabricante;
-	}
-	public void setPlaca(String placa) {
-		this.placa = placa;
-	}
 	public void setPuertas(int puertas) {
 		this.puertas = puertas;
+	}
+	public int getVelocidadMaxima() {
+		return velocidadMaxima;
 	}
 	public void setVelocidadMaxima(int velocidadMaxima) {
 		this.velocidadMaxima = velocidadMaxima;
 	}
+	public String getNombre() {
+		return nombre;
+	}
 	public void setNombre(String nombre) {
 		this.nombre = nombre;
+	}
+	public int getPrecio() {
+		return precio;
 	}
 	public void setPrecio(int precio) {
 		this.precio = precio;
 	}
+	public int getPeso() {
+		return peso;
+	}
 	public void setPeso(int peso) {
 		this.peso = peso;
+	}
+	public String getTraccion() {
+		return traccion;
 	}
 	public void setTraccion(String traccion) {
 		this.traccion = traccion;
 	}
+	public Fabricante getFabricante() {
+		return fabricante;
+	}
 	public void setFabricante(Fabricante fabricante) {
 		this.fabricante = fabricante;
 	}
-	
-	public int getCantidadVehiculos() {
-		return Cvehiculo;
+	public static int getCantidadVehiculos() {
+		return CantidadVehiculos;
+	}
+	public static void setCantidadVehiculos(int x) {
+		CantidadVehiculos=x;
+	}
+	public void vehiculosPorTipo() {
+		System.out.println("Automoviles: "+Automovil.cAutos+"\nCamionetas: "+Camioneta.Tcamioneta+"\nCamiones: "+Camion.cCamiones);
 	}
 	
-	public String vehiculosPorTipo() {
-		Automovil a= null;
-		Camion b= null;
-		Camioneta c = null;
-		return("Automoviles:"+ Automovil.cAutos+
-				"\nCamionetas:"+Camioneta.Tcamioneta+
-				"\nCamiones:"+ Camion.cCamiones);
-	}
 	
 	
 }
