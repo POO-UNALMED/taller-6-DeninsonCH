60c4ec4879c494475aa2f544316e36dc19ce7852
DeninsonCH <69688198+DeninsonCH@users.noreply.github.com>
2020-10-06 02:58:38 -0500

    q

--- src/main/java/vehiculos/Pais.java
+++ src/main/java/vehiculos/Pais.java
@@ -3,64 +3,40 @@ package vehiculos;
 import java.util.ArrayList;
 
 public class Pais {
-	private String nombre;
-	private int ventas;
-	static private ArrayList<Pais>paises=new ArrayList();
+	public String nombre;
+	static ArrayList <Pais> paises=new ArrayList <Pais>();
 	
 	public Pais(String nombre) {
-		this.nombre=nombre;
-		Pais a= new Pais(nombre);
-		
-		if(isPais(nombre)) {
-			for (int i = 0; i < paises.size(); i++) {
-				if(paises.get(i).getNombre().equals(nombre)){
-					paises.get(i).setVentas();
-					break;
-				}
-			}
-		}else {
-			a.setVentas();
-			paises.add(a);
-		}
-		
+		this.nombre = nombre;
+		paises.add(this);
 		
 	}
-
+	
 	public String getNombre() {
 		return nombre;
 	}
-
 	public void setNombre(String nombre) {
 		this.nombre = nombre;
 	}
-	public static String paisMasVendedor() {
-		Pais max=null;
-		for (int i = 0; i < paises.size(); i++) {
-			if (paises.get(i).ventas > max.ventas) {
-                max = paises.get(i);
-            }
+	
+	public static Pais paisMasVendedor() {
+		int contador=0;
+		int mayor=0;
+		Pais pais = null;
+		for (int i=0; i<paises.size();i++) {
+			for (int j=0; j<paises.size();j++) {
+				if(paises.get(i)==paises.get(j)) {
+					contador++;
+				}
+			}
+			if (contador>mayor) {
+				mayor=contador;
+				pais=paises.get(i);
+			}
+			contador=0;
 		}
-		return max.getNombre();
+		return pais;
 		
 	}
-	public int getVentas() {
-		return ventas;
-	}
-	public void setVentas() {
-		this.ventas=ventas++;
-	}
-	public boolean isPais(String p) {
-		boolean a=false;
-		for (int i = 0; i < paises.size(); i++) {
-			if(paises.get(i).getNombre().equals(p)){
-				a= true;
-				break;
-			}else {
-				a= false;
-			}
-		}
-		return a;
-	}
 	
-
 }
